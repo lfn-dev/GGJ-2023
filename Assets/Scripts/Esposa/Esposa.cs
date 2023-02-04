0ba44b9bc95cdc3f93ceebb46cfdbb8fb4fee954
LuscasD <73210639+LuscasD@users.noreply.github.com>
2023-02-04 10:14:14 -0300

    esposa

---
+++ Assets/Scripts/Esposa/Esposa.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Esposa : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask oqueEChao, oqueEPlayer;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    bool jaACasou;
+
+    public float sightRange, attackRange;
+    public bool playerIsInSigthRange, playerInAttackRange;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerIsInSigthRange = Physics.CheckSphere(transform.position, sightRange, oqueEPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, oqueEPlayer);
+
+        if (!playerIsInSigthRange && !playerInAttackRange) Patroling();
+        if (playerIsInSigthRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerIsInSigthRange) Casaram();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SceareachWalkingPoint();
+        if (walkPointSet) agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkingPoint = transform.position - walkPoint;
+
+        if (distanceToWalkingPoint.magnitude < 1f)
+            walkPoint = false;
+
+    }
+
+    public void SceareachWalkingPoint()
+    {
+        float randomz = Random.Range(-walkPointRange, walkPointRange);
+        float randomx = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(trasform.position.x + randomx, transform.position.y, transform.position.z + randomz);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, oqueEChao))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Casaram()
+    {
+
+    }
+}
