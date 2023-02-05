845f9de915088e6c09bf280a3c0442eb089e01e4
LuscasD <73210639+LuscasD@users.noreply.github.com>
2023-02-04 21:40:55 -0300

    LucasUnity Update Filho

--- Assets/Scripts/Filho/Filho.cs
+++ Assets/Scripts/Filho/Filho.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Filho : MonoBehaviour
 {
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(player.position);
     }
 
     // Update is called once per frame
